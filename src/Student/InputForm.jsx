776ec2ee670bbba6b585fe2f9ed797d38e763443
thanhlv <vinhthanhluong@gmail.com>
2025-07-16 23:30:12 +0700

    update validation

--- src/Student/InputForm.jsx
+++ src/Student/InputForm.jsx
@@ -19,17 +19,89 @@ export default function InputForm() {
     values: {
       id: "",
       name: "",
+      phone: "",
+      email: "",
     },
-    error: {
+    errors: {
       id: "",
       name: "",
+      phone: "",
+      email: "",
     },
   });
+
+  const handleValue = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    setValidation({
+      ...validation,
+      values: {
+        ...validation.values,
+        [name]: value,
+      },
+    });
+  };
+  const handleErrors = (e) => {
+    const { name, value } = e.target;
+
+    let mess = value.trim() == "" ? `Không được để trống  ${name}` : "";
+
+    if (value) {
+      switch (name) {
+        case "id":
+          // eslint-disable-next-line no-case-declarations
+          const regexId = /^[0-9]+$/;
+          if (!value.match(regexId)) {
+            mess = "Mã SV phải là số";
+          }
+          break;
+        case "name":
+          // eslint-disable-next-line no-case-declarations
+          const regexName =
+            /^[a-zA-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" + "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s]+$/;
+          if (!value.match(regexName)) {
+            mess = "Họ tên phải là chữ";
+          }
+          break;
+        case "phone":
+          // eslint-disable-next-line no-case-declarations
+          const regexPhone = /^[0-9]+$/;
+          if (!value.match(regexPhone)) {
+            mess = "Số điện thoại phải là số";
+          }
+          break;
+        case "email":
+          // eslint-disable-next-line no-case-declarations
+          const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+          if (!value.match(regexEmail)) {
+            mess = "Email không đúng định dạng";
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    setValidation({
+      ...validation,
+      errors: {
+        ...validation.errors,
+        [name]: mess,
+      },
+    });
+  };
 
   const handleAddStudent = () => {
     const { id, name, phone, email } = formData;
     const studentObj = new Student(id, name, phone, email);
     dispatch(addStudent({ ...studentObj }));
+
     setFormData({
       id: "",
       name: "",
@@ -60,23 +132,6 @@ export default function InputForm() {
       });
     }
   }, [dataObj]);
-
-  const handleValue = (e) => {
-    const { name, value } = e.target;
-
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-
-    setValidation({
-      ...validation,
-      values: {
-        ...validation.values,
-        [name]: value,
-      },
-    });
-  };
 
   return (
     <div className="mb-14" id="infoStudent">
@@ -135,7 +190,7 @@ export default function InputForm() {
               htmlFor="idSV"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
-              Mã SV
+              Mã SV <span className="text-red-600">*</span>
             </label>
             <input
               type="text"
@@ -144,15 +199,21 @@ export default function InputForm() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="maSV"
               onChange={handleValue}
+              onBlur={handleErrors}
               value={formData.id}
             />
+            {validation.errors.id && (
+              <div className="mt-2 text-sm text-red-800">
+                {validation.errors.id}
+              </div>
+            )}
           </div>
           <div>
             <label
               htmlFor="name"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
-              Họ tên
+              Họ tên <span className="text-red-600">*</span>
             </label>
             <input
               type="text"
@@ -161,15 +222,21 @@ export default function InputForm() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="Nguyễn Văn B"
               onChange={handleValue}
+              onBlur={handleErrors}
               value={formData.name}
             />
+            {validation.errors.name && (
+              <div className="mt-2 text-sm text-red-800">
+                {validation.errors.name}
+              </div>
+            )}
           </div>
           <div>
             <label
               htmlFor="phone"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
-              Số điện thoại
+              Số điện thoại <span className="text-red-600">*</span>
             </label>
             <input
               type="tel"
@@ -178,15 +245,21 @@ export default function InputForm() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="123-45-678"
               onChange={handleValue}
+              onBlur={handleErrors}
               value={formData.phone}
             />
+            {validation.errors.phone && (
+              <div className="mt-2 text-sm text-red-800">
+                {validation.errors.phone}
+              </div>
+            )}
           </div>
           <div>
             <label
               htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
-              Email address
+              Email address <span className="text-red-600">*</span>
             </label>
             <input
               type="email"
@@ -195,8 +268,14 @@ export default function InputForm() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
               placeholder="nguyenvanb@gmail.com"
               onChange={handleValue}
+              onBlur={handleErrors}
               value={formData.email}
             />
+            {validation.errors.email && (
+              <div className="mt-2 text-sm text-red-800">
+                {validation.errors.email}
+              </div>
+            )}
           </div>
         </div>
 
@@ -205,7 +284,13 @@ export default function InputForm() {
             <button
               onClick={handleAddStudent}
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-[28%] px-5 py-2.5 text-center "
+              disabled={
+                !formData.id ||
+                !formData.name ||
+                !formData.phone ||
+                !formData.email
+              }
+              className="disabled:bg-gray-500 disabled:cursor-no-drop text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-[28%] px-5 py-2.5 text-center "
             >
               Thêm sinh viên
             </button>
@@ -214,7 +299,7 @@ export default function InputForm() {
             <button
               onClick={handleEditStudent}
               type="submit"
-              className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-[28%] px-5 py-2.5 text-center "
+              className="disabled:bg-gray-500 disabled:cursor-no-drop text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-[28%] px-5 py-2.5 text-center "
             >
               Sửa sinh viên
             </button>
